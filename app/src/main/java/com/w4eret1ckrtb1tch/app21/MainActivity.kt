8eefd52f109bb0833cbb33627d9a2103ea2a5f67
ChapaevAnton <virsus2009@gmail.com>
2021-05-26 14:01:07 +0700

    first commit
    *add Regular top app bar and click listener
    *add Contextual action bar and click listener

--- app/src/main/java/com/w4eret1ckrtb1tch/app21/MainActivity.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app21/MainActivity.kt
@@ -2,6 +2,12 @@ package com.w4eret1ckrtb1tch.app21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +15,82 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        //Regular top app bar
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        topAppBar.setOnClickListener {
+            Toast.makeText(this, "Главной меню", Toast.LENGTH_LONG).show()
+        }
+
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.main_activity_top_bar_favorites -> {
+                    Toast.makeText(this, it.itemId.toString(), Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                R.id.main_activity_top_bar_search -> {
+                    Toast.makeText(this, it.itemId.toString(), Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                R.id.main_activity_top_bar_more -> {
+                    Toast.makeText(this, it.itemId.toString(), Toast.LENGTH_LONG).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        //Contextual action bar
+        val textView = findViewById<TextView>(R.id.main_activity_text_view)
+        var actionMode: ActionMode? = null
+
+        textView.setOnLongClickListener {
+
+            if (actionMode != null) return@setOnLongClickListener false
+
+            val callback = object : ActionMode.Callback {
+
+                override fun onCreateActionMode(p0: ActionMode?, menu: Menu?): Boolean {
+                    p0?.menuInflater?.inflate(R.menu.top_app_bar, menu)
+                    p0?.title = getString(R.string.contextual_bar)
+                    return true
+                }
+
+                override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                    return false
+                }
+
+                override fun onActionItemClicked(p0: ActionMode?, menuItem: MenuItem?): Boolean {
+                    return when (menuItem?.itemId) {
+
+                        R.id.main_activity_top_bar_search -> {
+                            Toast.makeText(this@MainActivity, menuItem.itemId.toString(), Toast.LENGTH_LONG).show()
+                            true
+                        }
+                        R.id.main_activity_top_bar_favorites -> {
+                            Toast.makeText(this@MainActivity, menuItem.itemId.toString(), Toast.LENGTH_LONG).show()
+                            true
+                        }
+                        R.id.main_activity_top_bar_more -> {
+                            Toast.makeText(this@MainActivity, menuItem.itemId.toString(), Toast.LENGTH_LONG).show()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+
+                override fun onDestroyActionMode(p0: ActionMode?) {
+                    actionMode = null
+                }
+
+            }
+
+            actionMode = startActionMode(callback)
+
+            return@setOnLongClickListener true
+        }
 
     }
 }
