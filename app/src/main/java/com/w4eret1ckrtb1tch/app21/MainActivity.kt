1fc35aae536c9a5dff854598886e2ec1f32e6fe5
Chapaev Anton <virsus2009@gmail.com>
2021-05-29 12:44:03 +0700

    add style button
    *dynamic selection of the style of the created button

--- app/src/main/java/com/w4eret1ckrtb1tch/app21/MainActivity.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app21/MainActivity.kt
@@ -2,12 +2,8 @@ package com.w4eret1ckrtb1tch.app21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ActionMode
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.ToggleButton
+import android.view.*
+import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -16,6 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //set style to button
+        val relativeContainer = findViewById<RelativeLayout>(R.id.relativeContainer)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val btn = Button(ContextThemeWrapper(this,R.style.RoundButton))
+            btn.apply { text="my button" }
+            relativeContainer.removeAllViews()
+            relativeContainer.addView(btn)
+        }
+
+        //selected dark mode
         val buttonToggle = findViewById<ToggleButton>(R.id.buttonToggle)
         buttonToggle.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -103,6 +111,7 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onDestroyActionMode(p0: ActionMode?) {
                     actionMode = null
+
                 }
 
             }
