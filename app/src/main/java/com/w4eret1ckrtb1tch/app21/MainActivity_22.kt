261be65b171ea623eb33ed3824851801d6b159ea
Chapaev Anton <virsus2009@gmail.com>
2021-06-06 13:58:31 +0700

    22.2. View Animation

--- app/src/main/java/com/w4eret1ckrtb1tch/app21/MainActivity_22.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app21/MainActivity_22.kt
@@ -1,13 +1,37 @@
 package com.w4eret1ckrtb1tch.app21
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity_22 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_animation_main)
+        setContentView(R.layout.activity_rocket_start)
+
+        val rocket = findViewById<ImageView>(R.id.rocket)
+        val startRocket = AnimationUtils.loadAnimation(this, R.anim.animation_start_rocket)
+        rocket.setOnClickListener {
+            it.startAnimation(startRocket)
+        }
+
+
+//        val buttonRun = findViewById<Button>(R.id.button_run)
+//        val buttonTranslate = findViewById<Button>(R.id.button_translate)
+//        val buttonScale = findViewById<Button>(R.id.button_scale)
+//        val buttonRotate = findViewById<Button>(R.id.button_rotate)
+//        val animationTranslate = AnimationUtils.loadAnimation(this, R.anim.animation_translate)
+//        val animationScale = AnimationUtils.loadAnimation(this, R.anim.animation_scale)
+//        val animationRotate = AnimationUtils.loadAnimation(this, R.anim.animation_rotate)
+//
+//        buttonRun.setOnClickListener {
+//            buttonTranslate.startAnimation(animationTranslate)
+//            buttonScale.startAnimation(animationScale)
+//            buttonRotate.startAnimation(animationRotate)
+//        }
+
     }
 }
